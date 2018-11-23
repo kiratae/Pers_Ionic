274e46ea5a05d58e1c98a4eb980288edc530add6
Kiratae <kirataetwo@gmail.com>
2018-11-23 13:29:46 +0700

    edit question management page

--- src/app/question-management/question-management.page.ts
+++ src/app/question-management/question-management.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-question-management',
@@ -10,6 +10,11 @@ export class QuestionManagementPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  edit(item: any) {
+    console.log(item)
+    //item2.closeSlidingItems()
   }
 
 }
