ca3c74784c809808195764a5a7af6aecef631f8e
Kiratae <kirataetwo@gmail.com>
2018-11-30 01:25:33 +0700

    create subject service

--- src/app/question-management/question-management.page.ts
+++ src/app/question-management/question-management.page.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit} from '@angular/core';
-import { ItemSliding } from '@ionic/angular';
+import { ItemSliding, NavController, ToastController } from '@ionic/angular';
+import { QuestionService } from '../services/question.service'
+
+interface Meta {
+  table: string
+  type: string
+  total: number
+  total_entries: number
+}
+
+interface Question {
+    qt_id: number
+    qt_text: string
+    qt_status: number
+    qt_type: number
+  }
+
+// interface Subjects {
+//   sub_id: number
+//   sub_code_th: string
+//   sub_code_en: string
+//   sub_name_th: string
+//   sub_name_en: string
+//   sub_objective: string
+// }
 
 @Component({
   selector: 'app-question-management',
@@ -8,18 +32,58 @@ import { ItemSliding } from '@ionic/angular';
 })
 export class QuestionManagementPage implements OnInit {
 
-  private items: string[]
+  private questionLists: Question[];
+  private meta: Meta;
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+    private questionService: QuestionService,
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
-    this.items = ['item1', 'item2']
+    this.questionService.get_all().subscribe((response) => {
+      this.meta = response['meta']
+      console.log(this.meta.table)
+      this.questionLists = response['data']
+    },
+    err => {
+        console.log(err.type)
+        this.presentToast()
+    })
   }
 
-  edit(item: any, slidingItem: ItemSliding) {
-    console.log(item)
+  async presentToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'มีบางอย่างผิดพลาด! อภัยในความไม่สะดวก',
+      showCloseButton: true,
+      position: 'middle',
+      closeButtonText: 'ตกลง',
+      color: "danger"
+    });
+    toast.present();
+  }
+
+  back(){
+    this.navCtrl.navigateBack('');
+  }
+
+  view(id: any) {
+    console.log(`view: ${id}`)
+  }
+
+  add() {
+    console.log(`add`)
+  }
+
+  edit(id: any, slidingItem: ItemSliding) {
+    console.log(`edit: ${id}`)
     slidingItem.close();
-    //item2.closeSlidingItems()
+  }
+
+  delete(id: any, slidingItem: ItemSliding) {
+    console.log(`delete: ${id}`)
+    slidingItem.close();
   }
 
 }
---
+++ src/app/services/question.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  constructor(private http: Http) { }
+
+  get_all() {
+    return this.http.get('http://10.80.6.160:1045/que').pipe(map((res) => res.json())
+  }
+
+}
