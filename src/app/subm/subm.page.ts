3dfbfb93f4ddfd7f90a4abc723f618814a31019e
Thongthorn <59160177@go.buu.ac.th>
2018-12-15 12:14:36 +0700

    update

---
+++ src/app/subm/subm.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subm',
+  templateUrl: './subm.page.html',
+  styleUrls: ['./subm.page.scss'],
+})
+export class SubmPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
