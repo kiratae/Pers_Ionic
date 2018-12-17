60f3a9f80bf2022da7f672b1e8dc546a5746aad0
cokeend <coke09@live.com>
2018-12-18 01:55:50 +0700

    insert lv

--- src/app/services/level.service.ts
+++ src/app/services/level.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { map } from 'rxjs/operators';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+interface Level {
+  lv_name_th: string
+  lv_name_eng: string
+  lv_status: number
+}
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 
 @Injectable({
@@ -9,9 +20,14 @@ import { map } from 'rxjs/operators';
 export class LevelService {
 
   
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   get_all() {
-    return this.http.get('http://10.80.6.160:1045/lvl').pipe(map((res) => res.json()))
+    return this.http.get('http://10.80.6.160:1045/lvl')
+  }
+
+  insert(lv_name_th, lv_name_eng, lv_status) {
+    let data = { 'lv_name_th':lv_name_th, 'lv_name_eng':lv_name_eng, 'lv_status':lv_status }
+    this.http.post('http://10.80.6.160:1045/lvl', JSON.stringify(data))
   }
 }
