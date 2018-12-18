e2ef59d59d2448e1fb56c0bee6ab46ae2bec43da
cokeend <coke09@live.com>
2018-12-19 02:32:13 +0700

    app edit-lv page

---
+++ src/app/lvm/edit-lv/edit-lv.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EditLvPage } from './edit-lv.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EditLvPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [EditLvPage]
+})
+export class EditLvPageModule {}
