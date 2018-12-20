4caeb9b00dc3be63f1e0ff5bb17b35304e9c7a97
59160697 <59160697@go.buu.ac.th>
2018-12-20 19:56:44 +0700

    new obj

--- src/app/subm/chtm/schtm/objm/objm.page.ts
+++ src/app/subm/chtm/schtm/objm/objm.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemSliding, NavController, ToastController, LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-objm',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjmPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
   }
+  back(){
+    this.navCtrl.navigateBack('schtm/:id');
+  }
 
+  add(){
+    this.navCtrl.navigateForward('insert_obj');
+  }
 }
