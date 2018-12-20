bd0de337a4a1c986d004227cc635e33646cb41df
Kiratae <kirataetwo@gmail.com>
2018-12-20 21:07:25 +0700

    add choices modal

---
+++ src/app/qtm/cho-modal/cho-modal.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ChoModalPage } from './cho-modal.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ChoModalPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ChoModalPage]
+})
+export class ChoModalPageModule {}
