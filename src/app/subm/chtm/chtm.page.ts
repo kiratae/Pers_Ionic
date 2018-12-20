82b4486b9b7aa0143c63914f8e9cb8d7877e53e9
Best 012364 <59160174@go.buu.ac.th>
2018-12-20 17:48:47 +0700

    update chtm

--- src/app/subm/chtm/chtm.page.ts
+++ src/app/subm/chtm/chtm.page.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemSliding, NavController, ToastController } from '@ionic/angular';
+//import { ChapterService } from '../../services/chapter.service'
+
+//interface Chapter {
+  //cht_id: number
+  //cht_sequence: number
+  //cht_code: string
+  //cht_name: string
+  //cht_status: number
+  //cht_sub_id: number
+//}
 
 @Component({
   selector: 'app-chtm',
@@ -7,9 +18,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChtmPage implements OnInit {
 
-  constructor() { }
+  //private chapterLists: Chapter[];
 
-  ngOnInit() {
+  constructor(
+    private navCtrl: NavController,
+    //private chapterService: ChapterService,
+    private toastCtrl: ToastController
+  ) { }
+
+  //ngOnInit() {
+    //this.chapterService.fecth(this.cht_sub_id).subscribe((response) => {
+      //  this.meta = response['meta']
+      //  console.log(this.meta.table)
+      //  this.chapterService = response['data']
+      //},
+      //err => {
+        //  console.log(err.type)
+        //  this.errToast()
+      //})
+  //}
+
+  //async errToast() {
+    //const toast = await this.toastCtrl.create({
+      //message: 'มีบางอย่างผิดพลาด! อภัยในความไม่สะดวก',
+      //showCloseButton: true,
+      //position: 'middle',
+      //closeButtonText: 'ตกลง',
+      //color: "danger"
+    //});
+    //toast.present();
+  //}
+
+  back(){
+    this.navCtrl.navigateBack('subm');
   }
 
+  add(){
+    this.navCtrl.navigateForward('insert_chtm');
+  }
 }
