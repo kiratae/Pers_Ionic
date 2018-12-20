b76da868dc10e837dc53bfd2085cdf94242bb879
Best 012364 <59160174@go.buu.ac.th>
2018-12-20 19:02:53 +0700

    up date

---
+++ src/app/subm/chtm/schtm/objm/edit-objm/edit-objm.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-objm',
+  templateUrl: './edit-objm.page.html',
+  styleUrls: ['./edit-objm.page.scss'],
+})
+export class EditObjmPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/subm/chtm/schtm/objm/insert-obj/insert-obj.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-obj',
+  templateUrl: './insert-obj.page.html',
+  styleUrls: ['./insert-obj.page.scss'],
+})
+export class InsertObjPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
